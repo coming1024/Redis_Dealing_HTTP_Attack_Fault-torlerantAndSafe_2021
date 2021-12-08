de36664081310520ffe05f66eb01337e3b9be244
coming1024 <chengyihengok@126.com>
2021-12-08 10:39:30 +0800

    add function
    now project can restrict the interface to access

---
+++ src/main/java/com/example/mydemo/service/UserService.java
@@ -0,0 +1,20 @@
+package com.example.mydemo.service;
+import com.example.mydemo.pojo.BasicUser;
+import com.example.mydemo.pojo.User;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+//也是没有写对应的注解的，到时候可以改一下
+@Service
+public interface UserService {
+    //和mapper相呼应，有一个对应的方法
+    //List<User> getUserList();
+
+    //MyUsers regist(MyUsers myusers);
+    User test(String account);
+    boolean login(String account,String password);
+    boolean pushUserComment(String userEmail,String experimentID,String content);
+
+}
+
+
